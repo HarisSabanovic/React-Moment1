de2b8c1cb69adcf3d12a7ebb77f41fac582498a8
Haris Sabanovic <hasa2303@student.miun.se>
2025-01-28 15:24:34 +0100

    skapade en array med kamsport objekt

--- Moment1-React/src/App.tsx
+++ Moment1-React/src/App.tsx
@@ -5,6 +5,33 @@ import Card from './components/Card';
 function App() {
   const pageTitle = "Kampsporter";
 
+  const martialArtArray = [
+    {
+      martialArt: "Boxning",
+      info: "Boxning är en kampsport där två utövare slår mot varandra med bara händerna, vanligtvis i en ring. Målet är att slå motståndaren till marken eller samla fler poäng genom att träffa motståndaren."
+    },
+    {
+      martialArt: "Judo",
+      info: "Judo är en japansk kampsport som fokuserar på att kasta motståndaren till marken genom grepp och tekniker för att övervinna motståndaren genom vikt och rörelse."
+    },
+    {
+      martialArt: "Taekwondo",
+      info: "Taekwondo är en koreansk kampsport som betonar sparkar och snabba benrörelser. Den är känd för sina spektakulära och kraftfulla höga sparkar."
+    },
+    {
+      martialArt: "Muay Thai",
+      info: "Muay Thai, även kallad thailändsk boxning, är en kampsport som använder slag, sparkar, knän och armbågar. Den är känd för sin hårda och effektiva teknik."
+    },
+    {
+      martialArt: "Karate",
+      info: "Karate är en japansk kampsport som fokuserar på slag, sparkar, blockeringar och kata (förutbestämda rörelsemönster). Den betonar både självförsvar och disciplin."
+    },
+    {
+      martialArt: "Brazilian Jiu-Jitsu",
+      info: "Brazilian Jiu-Jitsu (BJJ) är en kampsport som fokuserar på markkamp och submissionstrick som leder till att motståndaren ger upp. Den är känd för sina effektiva tekniker för att besegra större och starkare motståndare."
+    }
+  ]
+
   return (
     <>
      <h1>{pageTitle}</h1>
